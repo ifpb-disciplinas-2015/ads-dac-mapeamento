48ba420b912f921f01f8602f106b6d2ef7d36242
Ricardo Job <sousajob@gmail.com>
2015-04-22 09:34:57 -0300

    adicionando interface grafica

--- src/main/java/ifpb/dac/contexto/Main.java
+++ src/main/java/ifpb/dac/contexto/Main.java
@@ -5,14 +5,23 @@ package ifpb.dac.contexto;
  */
 public class Main {
     public static void main(String[] args) {
-        Endereco endereco=new Endereco("manel");
+//        Endereco endereco=new Endereco("manel");
         DAO<Endereco> dao=new DAOJPA<>();
 //        if(dao.salvar(endereco))
 //                System.out.println("Deucerto");
 //        else
 //                System.out.println("Deu errado");
-        Endereco obj=dao.buscar(51, Endereco.class);
+        Endereco enderecoRetorno=dao.buscar(51, Endereco.class);
 //        System.out.println(obj);
+//        enderecoRetorno.setRua("joao");
+        if(dao.excluir(enderecoRetorno)){
+            System.out.println("Deu certo");
+        }else{
+            System.out.println("Nao deu certo");
+        }
+        
+        
+        
         
     }
 }
