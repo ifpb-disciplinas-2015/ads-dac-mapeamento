f054991254a365807bf1e70cce93bd4214c96754
Ricardo Job <sousajob@gmail.com>
2015-05-04 08:02:48 -0300

    Olha ai

--- src/main/java/ifpb/dac/contexto/Aluno.java
+++ src/main/java/ifpb/dac/contexto/Aluno.java
@@ -33,8 +33,8 @@ public class Aluno implements Serializable {
     @Transient
     private Matricula matricula;
 
-    @Access(AccessType.PROPERTY)
-    @Column(name = "matricula_aluno")
+//    @Access(AccessType.PROPERTY)
+//    @Column(name = "matricula_aluno")
     public String getMatriculaSimples() {
         return this.matricula.simples();
     }
